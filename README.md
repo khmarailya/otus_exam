e3dd085f11b885b06e54a60ee0b7ab2cedfed66d
hmara_iv <hmara_iv@magnit.ru>
2022-03-16 16:56:47 +0300

    setup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # otus_exam
+
+## linux
+
+__Стянуть проект__
+
+    cd /opt
+    git clone https://github.com/khmarailya/otus_exam.git
+    bash setup/env.sh
+
+__Поднять Селеноид__
+
+    cp -rf otus_exam/setup/selenoid/ selenoid
--- setup/env.sh
+++ setup/env.sh
@@ -10,15 +10,16 @@ fi
 
 echo "Подготовка виртуального окружения: ОС = $os"
 pip install virtualenv
-python -m venv env
 
 case "$os" in
   win) echo
+  python -m venv env
   source env/Scripts/activate.bat
   echo 'установка зависимостей'
   env/Scripts/pip install -r requirements.txt;;
 
   lin) echo
+  python3.9 -m venv env
   source env/bin/activate
   echo 'установка зависимостей'
   env/bin/pip install -r requirements.txt;;
